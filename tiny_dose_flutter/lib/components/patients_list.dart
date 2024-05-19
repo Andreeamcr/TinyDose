c86c7516277724966ad78b7624ff7f25cbbbafad
Andreea Mocrii <mocriiandreea@yahoo.com>
2024-05-19 20:20:26 +0300

    add search bar for patients list

--- tiny_dose_flutter/lib/components/patients_list.dart
+++ tiny_dose_flutter/lib/components/patients_list.dart
@@ -1,44 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:tiny_dose_flutter/screens/records_screen.dart';
-import 'package:tiny_dose_flutter/utils/constants.dart';
 
 class PatientsList extends StatelessWidget {
-  final List<String> items = [
-    'Item1',
-    'Item2',
-    'Item3',
-    'Item4',
-    'Item5',
-  ];
-  PatientsList({
+  // final List<String> items = [
+  //   'Popescu Adelin',
+  //   'Ionescu Marius-Adrian',
+  //   'Amariei Georgiana',
+  //   'Vasilescu Patricia',
+  //   'Lucaci Simion',
+  //   'asfbjasbfck',
+  //   '',
+  //   'sgs',
+  //   'gerujher',
+  //   'rherre',
+  //   'ytjrt',
+  //   'sgsg',
+  // ];
+  final List<String> items;
+  const PatientsList({
     super.key,
+    required this.items,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Pacienți înregistrați',
-          style: kRalewayBoldFont,
-        ),
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
-      body: ListView.builder(
-          itemCount: items.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(items[index]),
-              onTap: () {
-                Navigator.pushNamed(context, MedicalRecordScreen.id);
-              },
-            );
-          }),
-    );
+    return ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(items[index]),
+            onTap: () {
+              Navigator.pushNamed(context, MedicalRecordScreen.id);
+            },
+          );
+        });
   }
 }
--- tiny_dose_flutter/lib/screens/patients_screen.dart
+++ tiny_dose_flutter/lib/screens/patients_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:tiny_dose_flutter/components/patients_list.dart';
+import 'package:tiny_dose_flutter/utils/constants.dart';
 
 class PatientsListScreen extends StatefulWidget {
   static String id = 'patients';
@@ -10,8 +12,112 @@ class PatientsListScreen extends StatefulWidget {
 }
 
 class _PatientsListScreenState extends State<PatientsListScreen> {
+  List<String> items = [
+    'Popescu Adelin',
+    'Ionescu Marius-Adrian',
+    'Amariei Georgiana',
+    'Vasilescu Patricia',
+    'Lucaci Simion',
+    'asfbjasbfck',
+    '',
+    'sgs',
+    'gerujher',
+    'rherre',
+    'ytjrt',
+    'sgsg',
+  ];
+  List<String> filteredItems = [];
+  TextEditingController searchController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    filteredItems = items;
+    searchController.addListener(() {
+      filterItems();
+    });
+  }
+
+  void filterItems() {
+    setState(() {
+      if (searchController.text.isEmpty) {
+        filteredItems = items;
+      } else {
+        filteredItems = items
+            .where((item) => item
+                .toLowerCase()
+                .contains(searchController.text.toLowerCase()))
+            .toList();
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return PatientsList();
+    final double width = MediaQuery.of(context).size.width;
+    final double height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'Pacienți înregistrați',
+          style: kRalewayBoldFont,
+        ),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Row(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: height * 0.02,
+                ),
+                SizedBox(
+                  width: width * 0.5,
+                  height: height * 0.9,
+                  child: PatientsList(
+                    items: filteredItems,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            width: width * 0.05,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              SizedBox(
+                height: height * 0.05,
+              ),
+              SizedBox(
+                width: width * 0.3,
+                height: height * 0.1,
+                child: TextField(
+                  controller: searchController,
+                  decoration: const InputDecoration(
+                    labelText: 'Căutare după nume',
+                    border: OutlineInputBorder(),
+                    prefixIcon: Icon(Icons.search),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
